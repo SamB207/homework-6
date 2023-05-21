307f8dd807bf6d46f7a47e69367d1f9063d334f1
SamB207 <lovecita3@gmail.com>
2023-05-21 02:09:58 -0400

    send to github

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+//import path module
+const path = require('path')
+const app = express();
+const PORT = 8001
+
+// app.get('/api', (req, res)=>{
+//     res.json({message: "Yo yo!"})
+//      })
+
+app.listen(PORT, ()=>{
+    console.log(`App is running on port ${PORT}`)
+})
